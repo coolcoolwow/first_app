2e9dddc2f7b2a235beeb1e973d3070da3ea189ff
Marcus <marcusthornton2001@gmail.com>
2014-08-13 15:27:20 +0800

    adds follow ajax functions

--- app/assets/javascripts/follow_ajax.js
+++ app/assets/javascripts/follow_ajax.js
@@ -1,6 +1,32 @@
-$(function () {
-	$('li.user[user_id].btn').click(function(event) {
+$(function (){
+	$('aside.user[user_id], li.user[user_id]').find('.btn').click(function(event){
 		var $this = $(this);
-		var userId = $this.parent().attr('user_id');
-	})
-});fdsfdsfdsf
+		var userId = $this.parents().filter('.user').attr('user_id');
+		if ($(this).hasClass('btn-primary')){
+			$.ajax({
+				url: '/users/'+userId+'/follow',
+				type: 'POST',
+				success: function(data, status){
+					$this.html('Unfollow').removeClass('btn-primary');
+					$this.html('Unfollow').addClass('btn-info');
+				},
+				error: function( jqXHR, textStatus, errorThrown ){
+					alert(jqXHR+","+textStatus+","+errorThrown);
+				},
+
+			});
+		}
+		else{
+			$.ajax({
+				url: '/users/'+userId+'/unfollow',
+				type: 'DELETE',
+				success: function(data, status){
+					$this.html('Unfollow').removeClass('btn-info');
+					$this.html('Follow').addClass('btn-primary');
+				}
+			});
+		}
+		event.preventDefault();
+	});
+});
+
